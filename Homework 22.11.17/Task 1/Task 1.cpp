72791439980f278e85ba374cff01f0ba46045922
Max <dranik15@mail.ru>
2018-01-05 12:31:04 +0300

    fixed

--- Homework 22.11.17/Task 1/Task 1.cpp
+++ Homework 22.11.17/Task 1/Task 1.cpp
@@ -6,12 +6,15 @@ using namespace std;
 int main()
 {
 	char massive[N];
-	char newMassive[N] = "";
-
+	char newWords[N][N];
+	int k = 0;
 	cout << "Input string: \n";
 	cin.getline(massive, 255);
 
-	strCreateNewArray(massive, newMassive);
-	cout << "New string:  \n" << newMassive << endl;
+	strCreateNewArrayOfWords(massive, newWords, k);	
+	for (int i = 0; i < k; i++)
+	{
+		cout  << newWords[i] << "\n";
+	}
 	system("pause");
 }
